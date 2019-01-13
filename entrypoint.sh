f8f2ea1b5bcc85762d2979e5859c48fab0cc4b1c
Ren Hao <renhao2k@gmail.com>
2019-01-13 10:00:24 +0800

    update.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-if [[ -z $UID ]]; then
-    UID=1000
-fi
-
-if [[ -z $GID ]]; then
-    GID=1000
-fi
-
-usermod -u $UID www-data && groupmod -g $GID www-data
-
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
